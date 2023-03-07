6f68efe3eba3647de292f3555d59fac00d06634d
barrajac <108970552+barrajac@users.noreply.github.com>
2023-03-07 14:24:13 -0800

    Easy going answered

---
+++ answers.js
@@ -0,0 +1,116 @@
+////////////////////////////////
+// Easy Going
+// Write a for loop that will log the numbers 1 through 20.
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+// TODO: The commit message should read: "Easy Going answered"
+////////////////////////////////
+// Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+//  Hint: Think about the increment expression.
+// TODO: The commit message should read: "Get Even answered"
+
+////////////////////////////////
+
+////////////////////////////////
+// Fizz Buzz
+// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+/**
+ * Write a javascript application that logs all numbers from 1 - 100.
+ * If a number is divisible by 3 log "Fizz" instead of the number.
+ * If a number is divisible by 5 log "Buzz" instead of the number.
+ * If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+ */
+
+// TODO: The commit message should read: "Fizz Buzz answered"
+////////////////////////////////
+
+////////////////////////////////
+// Wild Wild Life
+//  Use the following arrays to answer the questions below (name,species ,age, hometown): You should be modifying the elements by accessing them. It is up to you which methods to use.
+
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+/**
+ * Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+ * Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+ * Give D'Art a second hometown by adding "Hawkins"
+ * Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+ */
+
+// TODO: The commit message should read: "Wild Wild Life answered"
+////////////////////////////////
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+/**
+ * Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+ * Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+ */
+
+// TODO: The commit message should read: "Yell at the Ninja Turtles answered"
+////////////////////////////////
+
+// //////////////////////////////
+// Methods, Revisited
+// here is a list of favMovies:
+// const favMovies = [
+//   "Jaws",
+//   "The Fellowship of the Ring",
+//   "Howl's Moving Castle",
+//   "Django Unchained",
+//   "Cloud Atlas",
+//   "The Usual Suspects",
+//   "Toy Story",
+//   "Conan the Barbarian",
+//   "Titanic",
+//   "Harry Potter",
+//   "Fried Green Tomatoes",
+//   "Volver",
+//   "Oculus",
+//   "Seven",
+//   "Black Panther",
+//   "Harry Potter",
+//   "Imitation of Life",
+//   "Snatch",
+//   "Fast and Furious",
+// ];
+
+// Console log: the index of Titanic
+//  Do the following and console.log the final results (I have included some thought questions, you don't have to write out an answer for those marked as such):
+
+// /**
+//  * use the .sortmethod Thought question: what did this do to the array? Did it permanently alter it?
+//  * Use the method pop
+//  * push"Guardians of the Galaxy"
+//  * Reverse the array
+//  * Use the shiftmethod
+//  * unshift- what does it return?
+//  * splice"Django Unchained" and add "Avatar" (try finding the index of "Django Unchained", instead of counting it yourself)
+//  * Thought question: did this permanently alter our array?
+//  * slicethe last half of the array (challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it) - Thought question: did this permanently alter our array?
+//  * store the value of your slicein a variable, console.log it - Thought question: what is going on here?
+//  * console.log your final results
+//  * After running the above tasks, console.log the index of "Fast and Furious" -We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+//  * Thought question: that we declared the variable favMovieswith const, and yet, we were allowed to change the array. Weird? Should we have used let?
+//  * /
+// //  TODO: The commit message should read: "Methods Revisited answered"
+// //////////////////////////////
+
+// //////////////////////////////
+// Where is Waldo
+// //////////////////////////////
+
+// //////////////////////////////
+//  Excited Kitten
+// //////////////////////////////
+
+// //////////////////////////////
+//  Find the Median
+// //////////////////////////////
